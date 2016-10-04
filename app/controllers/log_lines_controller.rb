459bcfd0e2c4982d3bd3ae17f846056bf2ad435c
timlkelly <timlkelly@gmail.com>
2016-10-04 14:09:56 -0500

    require papertrail processor in create

--- app/controllers/log_lines_controller.rb
+++ app/controllers/log_lines_controller.rb
@@ -2,6 +2,7 @@ class LogLinesController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:create]
 
   def create
+    require 'papertrail_output_processor'
     # if params[:payload].present?
     PapertrailOutputProcessor.new.create_log_lines_from_events(params[:payload][:events])
     # else
