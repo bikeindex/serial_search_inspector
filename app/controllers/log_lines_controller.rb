4b7674c9e317bd9ddb1459c11a32001ab3ae2c3b
timlkelly <timlkelly@gmail.com>
2016-10-04 14:13:57 -0500

    add printout to create

--- app/controllers/log_lines_controller.rb
+++ app/controllers/log_lines_controller.rb
@@ -3,6 +3,7 @@ class LogLinesController < ApplicationController
 
   def create
     require 'papertrail_output_processor'
+    pp params[:payload]
     # if params[:payload].present?
     PapertrailOutputProcessor.new.create_log_lines_from_events(params[:payload][:events])
     # else
