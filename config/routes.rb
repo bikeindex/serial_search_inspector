0b41ff92e8fec1d150d36f76cc3d40aebd7a2acf
timlkelly <timlkelly@gmail.com>
2016-10-05 11:02:47 -0500

    add sidekiq web support

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
-# require 'sidekiq/web'
+require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  # mount Sidekiq::Web => '/sidekiq'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  mount Sidekiq::Web => '/sidekiq'
   root to: 'serial_searches#index'
-
-  resources :serial_searches, only: [:index, :show]
   resources :log_lines, only: [:create]
+  resources :serial_searches, only: [:index, :show]
 end
