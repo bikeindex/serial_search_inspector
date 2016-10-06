8486ae3992ae5bbcecb0ab98bb0e84aa2bc67277
timlkelly <timlkelly@gmail.com>
2016-10-06 13:08:30 -0500

    add log api check

--- app/controllers/log_lines_controller.rb
+++ app/controllers/log_lines_controller.rb
@@ -3,11 +3,15 @@ class LogLinesController < ApplicationController
 
   def create
     require 'papertrail_output_processor'
-    parsed_events = JSON.parse(params[:payload]).with_indifferent_access[:events]
-    # if params[:payload].present?
-    PapertrailOutputProcessor.new.create_log_lines_from_events(parsed_events)
-    # else
-    #   render json: 'Error: Invalid Payload', status: :bad_request and return
-    # end
+    if params[:source] == ENV['LOG_ORIGIN']
+      parsed_events = JSON.parse(params[:payload]).with_indifferent_access[:events]
+      # if params[:payload].present?
+      PapertrailOutputProcessor.new.create_log_lines_from_events(parsed_events)
+      # else
+      #   render json: 'Error: Invalid Payload', status: :bad_request and return
+      # end
+    else
+      render json: 'Error: Invalid Log Source', status: :unauthorized and return
+    end
   end
 end
