09c0ff8ba0de89f41382490aca60b95999cd8890
timlkelly <timlkelly@gmail.com>
2016-10-07 17:29:53 -0500

    add sort arrows

--- app/jobs/update_serial_search_last_request_at_job.rb
+++ app/jobs/update_serial_search_last_request_at_job.rb
@@ -2,6 +2,7 @@ class UpdateSerialSearchLastRequestAtJob < ApplicationJob
   queue_as :default
 
   def perform(serial_search)
+    # check if maximum has changed then directly set attribute
     serial_search.update_attribute(:last_request_at, serial_search.log_lines.maximum(:request_at))
   end
 end
