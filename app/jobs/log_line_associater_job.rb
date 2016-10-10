cd1efb7251868381fd1c33d3b42eb60c7a2bbdc2
timlkelly <timlkelly@gmail.com>
2016-10-10 15:17:04 -0500

    heroku recent time

--- app/jobs/log_line_associater_job.rb
+++ app/jobs/log_line_associater_job.rb
@@ -2,7 +2,7 @@ class LogLineAssociaterJob < ApplicationJob
   queue_as :default
 
   def perform
-    LogLine.processed.each do |log_line|
+    LogLine.all.each do |log_line|
       log_line.find_or_create_ip_address_association
       log_line.find_or_create_serial_search_association
       log_line.save
