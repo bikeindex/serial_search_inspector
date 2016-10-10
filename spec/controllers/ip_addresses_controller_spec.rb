008106a59b3da80f9ca0c67127d9dd0c036519bc
timlkelly <timlkelly@gmail.com>
2016-10-10 14:25:26 -0500

    add sorting test for ip_address

--- spec/controllers/ip_addresses_controller_spec.rb
+++ spec/controllers/ip_addresses_controller_spec.rb
@@ -9,6 +9,20 @@ RSpec.describe IpAddressesController, type: :controller do
       expect(response).to render_template('index')
       expect(assigns(:ip_addresses)).to eq([ip_address])
     end
+    context 'sort by number of searches' do
+      let!(:ip_address_3) { FactoryGirl.create(:ip_address) }
+      let!(:ip_address_2) { FactoryGirl.create(:ip_address) }
+      before do
+        FactoryGirl.create(:log_line, ip_address: ip_address_2)
+        FactoryGirl.create(:log_line, ip_address: ip_address_3)
+        FactoryGirl.create(:log_line, ip_address: ip_address_3)
+      end
+      it 'sorts correctly by number of searches' do
+        get :index, sort: 'log_lines_count', direction: 'desc', per_page: '1'
+        expect(response.status).to eq 200
+        expect(assigns(:ip_addresses)).to eq([ip_address_3])
+      end
+    end
   end
 
   describe 'GET show' do
