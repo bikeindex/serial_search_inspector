63ab391e27018b0862d57bbac6135865d79609bb
Tim Kelly <timlkelly@users.noreply.github.com>
2016-10-10 11:17:43 -0500

    add sorting to table (#17)
    * add sort arrows
    
    * add second arrow
    
    * add sortable arrows
    
    * style arrows

--- app/controllers/ip_addresses_controller.rb
+++ app/controllers/ip_addresses_controller.rb
@@ -1,9 +1,11 @@
 class IpAddressesController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
     @title = 'IP Addresses'
     per_page = params[:per_page] || 50
     page = params[:page] || 1
-    @ip_addresses = IpAddress.page(page).per(per_page)
+    @ip_addresses = IpAddress.page(page).per(per_page).order(sort_column + ' ' + sort_direction)
     @serial_searches_count = SerialSearch.count
     @ip_addresses_count = IpAddress.count
   end
@@ -11,5 +13,15 @@ class IpAddressesController < ApplicationController
   def show
     @ip_address = IpAddress.find(params[:id])
     @title = "Searches from #{@ip_address.address}"
+  end
+
+  private
+
+  def sort_column
+    IpAddress.column_names.include?(params[:sort]) ? params[:sort] : 'address'
+  end
+
+  def sort_direction
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : 'asc'
   end
 end
--- app/controllers/serial_searches_controller.rb
+++ app/controllers/serial_searches_controller.rb
@@ -1,9 +1,11 @@
 class SerialSearchesController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
     @title = 'Serial Searches'
     per_page = params[:per_page] || 50
     page = params[:page] || 1
-    @serial_searches = SerialSearch.page(page).per(per_page)
+    @serial_searches = SerialSearch.page(page).per(per_page).order(sort_column + ' ' + sort_direction)
     @serial_searches_count = SerialSearch.count
     @ip_addresses_count = IpAddress.count
   end
@@ -11,5 +13,15 @@ class SerialSearchesController < ApplicationController
   def show
     @serial_search = SerialSearch.find(params[:id])
     @title = "Serial Searches for #{@serial_search.serial}"
+  end
+
+  private
+
+  def sort_column
+    SerialSearch.column_names.include?(params[:sort]) ? params[:sort] : 'serial'
+  end
+
+  def sort_direction
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : 'asc'
   end
 end
