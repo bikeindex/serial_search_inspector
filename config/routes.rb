433301006897d19875cd8e7b7bd554e92f09cc1e
timlkelly <timlkelly@gmail.com>
2016-10-11 17:11:13 -0500

    heroku graphs undefined

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   root to: 'serial_searches#index'
 
+  resources :log_lines, only: [:create]
+  resources :ip_addresses, only: [:index, :show]
+  resources :serial_searches, only: [:index, :show]
   resources :graphs, only: [:index] do
     collection do
       get 'source_type'
       get 'uniquely_created_entries'
     end
   end
-  resources :log_lines, only: [:create]
-  resources :ip_addresses, only: [:index, :show]
-  resources :serial_searches, only: [:index, :show]
 end
