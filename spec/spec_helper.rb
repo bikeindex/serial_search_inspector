d2ae073b19f46833e8016186864e5fc37ba15fbf
Tim Kelly <timlkelly@gmail.com>
2018-04-21 21:24:19 -0500

    Setup FactoryBot inside spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'factory_bot_rails'
-require 'support/factory_bot'
 
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
